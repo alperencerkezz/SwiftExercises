1fd35125262608f552cb11c61c7866f8930d3e85
Alperen Çerkez <alperencerkezz@gmail.com>
2024-11-02 00:30:27 +0300

    tab bar controller

---
+++ Controllers2/Controllers2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Controllers2
+//
+//  Created by Alperen Çerkez on 1.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

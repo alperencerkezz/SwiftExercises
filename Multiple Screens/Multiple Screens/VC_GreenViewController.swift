3f1b10b465f42ce514ce0b9e6c3e868482680079
Alperen Çerkez <alperencerkezz@gmail.com>
2024-11-04 22:29:00 +0300

    multiple screens

---
+++ Multiple Screens/Multiple Screens/VC_GreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VC_GreenViewController.swift
+//  Multiple Screens
+//
+//  Created by Alperen Çerkez on 4.11.2024.
+//
+
+import UIKit
+
+class VC_GreenViewController: UIViewController {
+    @IBOutlet weak var lblGreen: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Multiple Screens/Multiple Screens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multiple Screens
+//
+//  Created by Alperen Çerkez on 4.11.2024.
+//
+
+import UIKit
+
+class Vc_Blue: UIViewController {
+
+    @IBOutlet weak var lblBlue: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

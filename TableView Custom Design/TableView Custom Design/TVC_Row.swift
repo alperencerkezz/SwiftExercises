20ef35040503f5fcc48b9c00287b569d7bcdef90
Alperen Çerkez <alperencerkezz@gmail.com>
2024-11-07 23:27:45 +0300

    custom design

---
+++ TableView Custom Design/TableView Custom Design/TVC_Row.swift
@@ -0,0 +1,24 @@
+//
+//  TVC_Row.swift
+//  TableView Custom Design
+//
+//  Created by Alperen Çerkez on 7.11.2024.
+//
+
+import UIKit
+
+class TVC_Row: UITableViewCell {
+
+    @IBOutlet weak var lblText: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

761ebae5e3091017c291c977bd42c012db6a624e
Alperen Çerkez <alperencerkezz@gmail.com>
2024-11-07 16:46:04 +0300

    last adjustments

--- Picker View/Picker View/ViewController.swift
+++ Picker View/Picker View/ViewController.swift
@@ -11,11 +11,14 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
 
     @IBOutlet weak var pvList: UIPickerView!
     
-    var list = ["Element 1","Element 2","Element 3","Element 4","Element 5","Element 6","Element 7",]
+    var list = ["Element 1","Element 2","Element 3","Element 4","Element 5","Element 6","Element 7"]
+    var list2 = ["E 1","E 2","E 3","E 4","E 5","E 6","E 7"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        pvList.selectRow(1, inComponent: 0, animated: true)
+        pvList.selectRow(3, inComponent: 1, animated: true)
     }
 
     @IBAction func btnUpdate_TUI(_ sender: Any) {
@@ -23,19 +26,38 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         pvList.reloadAllComponents()
     }
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
+        return 2
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return list.count
+        
+        if component == 0 {
+            return list.count
+        }
+            
+        
+        return list2.count
     }
     
     func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
-        return NSAttributedString(string: list[row], attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
+        
+        if component == 0 {
+            return NSAttributedString(string: list[row], attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
+        }
+        
+        return NSAttributedString(string: list2[row], attributes: [NSAttributedString.Key.foregroundColor: UIColor.blue])
+        
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print(list[row])
+        
+        if component == 0 {
+            print(list[row])
+        } else {
+            print(list2[row])
+        }
+        
+        
     }
     
 }
